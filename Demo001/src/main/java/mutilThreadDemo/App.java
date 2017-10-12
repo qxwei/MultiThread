a9470ba9ce14edc4d9f6516d2aa15850ec66926b
IT小君 <121440270@qq.com>
2017-10-12 14:10:01 +0800

    添加demo项目

--- Demo001/src/main/java/mutilThreadDemo/App.java
+++ Demo001/src/main/java/mutilThreadDemo/App.java
@@ -6,8 +6,19 @@ package mutilThreadDemo;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println("虚拟机内核数："+Runtime.getRuntime().availableProcessors());
+        Runnable r = ()->{
+            Thread th = Thread.currentThread();
+            System.out.printf("我是：%s, 我的优先级:%s,是否存活：%s,当前状态：%s\n",
+                    th.getName(),th.getPriority(),th.isAlive(),th.getState());
+        };
+        Thread th1 = new Thread(r,"线程1号");
+        Thread th2 = new Thread(r,"线程2号");
+        th1.start();
+        th2.start();
+
+        Thread.sleep(1000);
     }
 }
