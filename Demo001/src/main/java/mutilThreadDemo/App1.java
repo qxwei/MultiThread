fb01a5cb9f245999c61e09fc11992d6da19a1643
IT小君 <121440270@qq.com>
2017-10-16 22:36:10 +0800

    run config

---
+++ Demo001/src/main/java/mutilThreadDemo/App1.java
@@ -0,0 +1,35 @@
+package mutilThreadDemo;
+
+/**
+ * Created by damon on 2017/10/16.
+ */
+public class App1 {
+    public static void main( String[] args ) throws Exception
+    {
+        System.out.println("虚拟机内核数："+Runtime.getRuntime().availableProcessors());
+        Runnable r = ()->{
+            Thread self = Thread.currentThread();
+            self.setName("子线程1号");
+            threadRun();
+        };
+        Thread childThread = new Thread(r);
+        childThread.start();
+
+        Thread self = Thread.currentThread();
+        self.setName("主线程main");
+        threadRun();
+
+    }
+
+    private static void threadRun(){
+        try {
+            for (int i = 0; i < 10; i++) {
+                int time = (int) (Math.random() * 1000);
+                Thread.sleep(time);
+                System.out.println(Thread.currentThread().getName()+"--Running !");
+            }
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
